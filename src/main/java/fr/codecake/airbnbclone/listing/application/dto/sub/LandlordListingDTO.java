c7386aab828ac5ca6682c080b83ffcc617ac51cc
robim16 <carteagajimenez@gmail.com>
2025-08-09 16:26:11 -0500

    creacion del ListingInfoDTO

---
+++ src/main/java/fr/codecake/airbnbclone/listing/application/dto/sub/LandlordListingDTO.java
@@ -0,0 +1,6 @@
+package fr.codecake.airbnbclone.listing.application.dto.sub;
+
+import jakarta.validation.constraints.NotNull;
+public record LandlordListingDTO(@NotNull String firstname,
+                                 @NotNull String imageUrl) {
+}
---
+++ src/main/java/fr/codecake/airbnbclone/user/application/UserException.java
@@ -0,0 +1,7 @@
+package fr.codecake.airbnbclone.user.application;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+}
